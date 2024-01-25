873977be2f11783fa01f2f29435d96c1bd4531db
Nyheim Hunter <78383071+Nyheimh@users.noreply.github.com>
2024-01-25 15:01:52 -0800

    Created a hook, moved the CSS styles

---
+++ plant-frontend/src/hooks/useModal.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const useModal = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpen(false);
+  };
+
+  return {
+    open,
+    handleOpenModal,
+    handleCloseModal,
+  };
+};
+
+export default useModal;
--- plant-frontend/src/screens/Monstera/Monstera.js
+++ plant-frontend/src/screens/Monstera/Monstera.js
@@ -11,81 +11,23 @@ import {
   Backdrop,
   Fade,
 } from "@material-ui/core";
+import useModal from "../../hooks/useModal";
 
-const useStyles = makeStyles((theme) => ({
-  blogsContainer: {
-    paddingTop: theme.spacing(5),
-    fontWeight: 800,
-    paddingBottom: theme.spacing(10),
-  },
-  monsteraCard: {
-    maxWidth: "100%",
-  },
-  monsteraCardMedia: {
-    height: 240,
-  },
-  cardActions: {
-    display: "flex",
-    margin: "0 10px",
-    justifyContent: "space-between",
-  },
-  h5: {
-    color: "#252e48",
-    fontSize: "18px",
-    lineHeight: " 24px",
-  },
-  monsteraCardModal: {
-    display: "flex",
-    alignItems: "center",
-    marginLeft: 500,
-    width: 500,
-  },
-  monsteraModalPaper: {
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-    justifyContent: "center",
-    alignContent: "center",
-  },
-  monsteraCardTitle: {
-    display: "flex",
-    alignContent: "center",
-    justifyContent: "center",
-    padding: 10,
-  },
-  monsteraSearchBar: {
-    border: "1px solid green",
-    borderRadius: 40,
-    margin: 20,
-    height: 40,
-    width: 300,
-    textAlign: "center",
-    fontSize: 15,
-    // backgroundColor: "gray",
-  },
-  monsteraSearchBarContainer: {
-    alignContent: "center",
-    alignItems: "center",
-    display: "flex",
-    justifyContent: "center",
-  },
-}));
-
-function Monstera(props) {
-  const { monsteras } = props;
+function Monstera({ monsteras }) {
+  // const { monsteras } = props;
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
+  const { open, handleOpenModal, handleCloseModal } = useModal();
+  // const [open, setOpen] = useState(false);
   const [searchMonstera, setSearchMonstera] = useState("");
   const [filteredMonsteras, setFilteredMonsteras] = useState(monsteras);
 
-  const handleOpenModal = () => {
-    setOpen(true);
-  };
+  // const handleOpenModal = () => {
+  //   setOpen(true);
+  // };
 
-  const handleCloseModal = () => {
-    setOpen(false);
-  };
+  // const handleCloseModal = () => {
+  //   setOpen(false);
+  // };
 
   const handleSearchChange = (e) => {
     const searchTerm = e.target.value.toLowerCase();
@@ -152,3 +94,63 @@ function Monstera(props) {
   );
 }
 export default Monstera;
+
+const useStyles = makeStyles((theme) => ({
+  blogsContainer: {
+    paddingTop: theme.spacing(5),
+    fontWeight: 800,
+    paddingBottom: theme.spacing(10),
+  },
+  monsteraCard: {
+    maxWidth: "100%",
+  },
+  monsteraCardMedia: {
+    height: 240,
+  },
+  cardActions: {
+    display: "flex",
+    margin: "0 10px",
+    justifyContent: "space-between",
+  },
+  h5: {
+    color: "#252e48",
+    fontSize: "18px",
+    lineHeight: " 24px",
+  },
+  monsteraCardModal: {
+    display: "flex",
+    alignItems: "center",
+    marginLeft: 500,
+    width: 500,
+  },
+  monsteraModalPaper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  monsteraCardTitle: {
+    display: "flex",
+    alignContent: "center",
+    justifyContent: "center",
+    padding: 10,
+  },
+  monsteraSearchBar: {
+    border: "1px solid green",
+    borderRadius: 40,
+    margin: 20,
+    height: 40,
+    width: 300,
+    textAlign: "center",
+    fontSize: 15,
+    // backgroundColor: "gray",
+  },
+  monsteraSearchBarContainer: {
+    alignContent: "center",
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+  },
+}));
